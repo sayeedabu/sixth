083ea22c62acb8fccda2968bb221c2311310a21c
Sayeed <sayeed@diit.info>
2015-04-02 16:07:52 +0600

    second commit

---
+++ src/SecondSixClass.java
@@ -0,0 +1,13 @@
+
+public class SecondSixClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test Git 6");
+		System.out.println("Second Sixth Class");
+
+	}
+
+}
